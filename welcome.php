8c9b67d0bcce578fc9df2a9b6b1de9dfab579b4d
Sergio Flores <sergioflores836@gmail.com>
2016-02-20 13:32:19 -0600

    YOLO

--- welcome.php
+++ welcome.php
@@ -10,21 +10,21 @@ $name =  $_GET["name"];
 
 $command = 'pdflatex template_a.tex';
 $output = shell_exec($command);
-//echo "<pre>$output</pre>";
+echo "<pre>$output</pre>";
 
 
 
 //echo ('pdflatex template_a.tex ' . $name); 
 
-$path = 'template_a.pdf';
-header("Content-Length: " . filesize ( $path ) ); 
-header("Content-type: application/pdf"); 
-header("Content-disposition: inline; filename=".basename($path));
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-ob_clean();
-flush();
-readfile($path);
+// $path = 'template_a.pdf';
+// header("Content-Length: " . filesize ( $path ) ); 
+// header("Content-type: application/pdf"); 
+// header("Content-disposition: inline; filename=".basename($path));
+// header('Expires: 0');
+// header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+// ob_clean();
+// flush();
+// readfile($path);
 
 ?>
 
