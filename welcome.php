cf90e200608ac71b188ca02fd94229e7e5fe2f57
Sergio Flores <sergioflores836@gmail.com>
2016-02-20 11:47:20 -0600

    YOLO

--- welcome.php
+++ welcome.php
@@ -4,38 +4,9 @@
 
 <h1>My first PHP page</h1>
 
-<?php
-echo $_GET["name"];
-
-?>
+<?php echo $_GET["name"]; ?>
 
 </body>
 </html>
 
 
-
-<html>
-
-<body>
-<?php
-$name = echo $_GET["name"];
-echo "$name";
-?>
-	 
-</body>
-</html>
-<?php 
-
-// $output = shell_exec('pdflatex command_test.tex ' + $name);
-//echo ('pdflatex command_test.tex ' + $name); 
-// $path = 'command_test.pdf';
-// header("Content-Length: " . filesize ( $path ) ); 
-// header("Content-type: application/pdf"); 
-// header("Content-disposition: inline; filename=".basename($path));
-// header('Expires: 0');
-// header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-// ob_clean();
-// flush();
-// readfile($path);
-
-?>
