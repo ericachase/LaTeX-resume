61a5436269fe3e2e447e8543fbb3bf11131808e6
root <sergioflores836@gmail.com>
2016-02-20 12:16:37 -0500

    syncing up with server files

--- welcome.php
+++ welcome.php
@@ -7,4 +7,15 @@ Welcome <?php echo $_GET["name"]; ?>
 <?php
 $output = shell_exec('ls -lart');
 echo "<pre>$output</pre>";
+
+$path = 'test.pdf';
+header("Content-Length: " . filesize ( $path ) ); 
+header("Content-type: application/pdf"); 
+header("Content-disposition: inline; filename=".basename($path));
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+ob_clean();
+flush();
+readfile($path);
+
 ?>
