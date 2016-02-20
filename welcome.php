f12a2e467ebd7bc9267d1d2863ad6ff3a0ffbc75
Erica Chase <ericachase11@gmail.com>
2016-02-20 14:02:44 -0600

    updated tex

--- welcome.php
+++ welcome.php
@@ -1,21 +1,34 @@
+<!DOCTYPE html>
 <html>
-
 <body>
+
+<h1>My first PHP page</h1>
+
+
+<?php 
+$name =  $_GET["name"];
+
+$command = 'pdflatex test.tex';
+$output = shell_exec($command);
+echo "<pre>$output</pre>";
+
+
+
+//echo ('pdflatex template_.tex ' . $name); 
+
+// $path = 'test.pdf';
+// header("Content-Length: " . filesize ( $path ) ); 
+// header("Content-type: application/pdf"); 
+// header("Content-disposition: inline; filename=".basename($path));
+// header('Expires: 0');
+// header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+// ob_clean();
+// flush();
+// readfile($path);
+
+?>
 
 </body>
 </html>
-<?php 
-$name = echo $_GET["name"]; 
-$output = shell_exec('pdflatex template_a.tex ' + $name);
 
-$path = 'template_a.pdf';
-header("Content-Length: " . filesize ( $path ) ); 
-header("Content-type: application/pdf"); 
-header("Content-disposition: inline; filename=".basename($path));
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-ob_clean();
-flush();
-readfile($path);
 
-?>
