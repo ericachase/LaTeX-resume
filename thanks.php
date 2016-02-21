f3e21089e1fdf796c3d237341b3396f83981262b
Sergio Flores <sergioflores836@gmail.com>
2016-02-21 01:07:54 -0600

    Added php download page and fixed style

--- thanks.php
+++ thanks.php
@@ -25,3 +25,16 @@
 	</footer>
 </body>
 </html>
+
+
+<?php
+$path = 'resume.pdf';
+header("Content-Length: " . filesize ( $path ) ); 
+header("Content-type: application/pdf"); 
+header("Content-disposition: attachment; filename=".basename($path));
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+ob_clean();
+flush();
+readfile($path);
+?>
