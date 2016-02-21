e7adddc302b2a297e92f7c7d4623b92b724ed7c1
Sergio Flores <sergioflores836@gmail.com>
2016-02-20 18:59:45 -0600

    trying echo instead of cat to write to file

--- welcome.php
+++ welcome.php
@@ -11,21 +11,21 @@ echo $Content;
 shell_exec('rm database.csv');
 
 //cat-ing the contents of the GET to a db file
-shell_exec('cat' . ' ' . $Content . ' ' . '>' . ' ' . 'database.csv');
+shell_exec('echo' . ' ' . $Content . ' ' . '>' . ' ' . 'database.csv');
 
-// //calling the complie command
-// shell_exec('pdflatex' . ' ' . 'simpleres.tex' . ' ' . 'database.csv');
+//calling the complie command
+shell_exec('pdflatex' . ' ' . 'simpleres.tex' . ' ' . 'database.csv');
 
-// //$output = shell_exec('pdflatex command_test.tex ' + $name);
-// // echo ('pdflatex command_test.tex ' + $name); 
-// $path = 'simpleres.pdf';
-// header("Content-Length: " . filesize ( $path ) ); 
-// header("Content-type: application/pdf"); 
-// header("Content-disposition: inline; filename=".basename($path));
-// header('Expires: 0');
-// header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-// ob_clean();
-// flush();
-// readfile($path);
+//$output = shell_exec('pdflatex command_test.tex ' + $name);
+// echo ('pdflatex command_test.tex ' + $name); 
+$path = 'simpleres.pdf';
+header("Content-Length: " . filesize ( $path ) ); 
+header("Content-type: application/pdf"); 
+header("Content-disposition: inline; filename=".basename($path));
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+ob_clean();
+flush();
+readfile($path);
 
 ?>
