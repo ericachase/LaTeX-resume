52fc3f0f7b392618a9cbc5541694e4d3998fc234
Sergio Flores <sergioflores836@gmail.com>
2016-02-20 18:56:54 -0600

    debugging Content variable

--- welcome.php
+++ welcome.php
@@ -6,26 +6,26 @@ $email = $_GET['email'];
 $Content = "Name, Email\n";
 $Content .= "$name, $email\n";
 
-
+echo $Content;
 //resetting the db file on load.
 shell_exec('rm database.csv');
 
 //cat-ing the contents of the GET to a db file
 shell_exec('cat' . ' ' . $Content . ' ' . '>' . ' ' . 'database.csv');
 
-//calling the complie command
-shell_exec('pdflatex' . ' ' . 'simpleres.tex' . ' ' . 'database.csv');
+// //calling the complie command
+// shell_exec('pdflatex' . ' ' . 'simpleres.tex' . ' ' . 'database.csv');
 
-//$output = shell_exec('pdflatex command_test.tex ' + $name);
-// echo ('pdflatex command_test.tex ' + $name); 
-$path = 'simpleres.pdf';
-header("Content-Length: " . filesize ( $path ) ); 
-header("Content-type: application/pdf"); 
-header("Content-disposition: inline; filename=".basename($path));
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-ob_clean();
-flush();
-readfile($path);
+// //$output = shell_exec('pdflatex command_test.tex ' + $name);
+// // echo ('pdflatex command_test.tex ' + $name); 
+// $path = 'simpleres.pdf';
+// header("Content-Length: " . filesize ( $path ) ); 
+// header("Content-type: application/pdf"); 
+// header("Content-disposition: inline; filename=".basename($path));
+// header('Expires: 0');
+// header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+// ob_clean();
+// flush();
+// readfile($path);
 
 ?>
